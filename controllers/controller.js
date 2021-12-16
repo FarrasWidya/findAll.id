8c54b7e4bce185e0b5f301b6a4e44837ce972f7e
Farras Widya <farraswidyan@gmail.com>
2021-12-16 15:45:43 +0700

    kerangka controller

---
+++ controllers/controller.js
@@ -0,0 +1,54 @@
+const {Agent,Profile,User,Task} = require('../models/index');
+
+class Controller {
+
+    static homepage(req,res){
+      res.render('homepage')
+    }
+    static registerPage(req,res){
+      
+    }
+
+    static createUser(req,res){
+
+    }
+
+    static listProfile(req,res){
+
+    }
+
+    static listTask(req,res){
+
+    }
+
+    static detailTask(req,res){
+
+    }
+
+    static addTask(req,res){
+
+    }
+
+    static addNewTask(req,res){
+
+    }
+
+    static addAgent(req,res){
+
+    }
+
+    static addNewAgent(res,req){
+
+    }
+
+    static confirmPage(res,req){
+
+    }
+
+    static destroyTask(req,res){
+
+    }
+
+}
+
+module.exports = Controller
---
+++ router/index.js
@@ -0,0 +1,15 @@
+// const express = require('express')
+// const req = require('express/lib/request')
+// const indexRouter = express.Router()
+// const Controller = require('../controllers/jobController')
+
+
+// indexRouter.get('/', Controller.homepage)
+
+// const jobRouter = require('./jobRoute.js')
+// indexRouter.use('/', jobRouter)
+
+// const applicantRouter = require('./applicantRoute');
+// indexRouter.use('/',applicantRouter)
+
+// module.exports = indexRouter
